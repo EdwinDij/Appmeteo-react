8e26743f460c1c061150811e1bb5b82e428dbfc2
EdwinDij <edwin.d899@gmail.com>
2022-06-19 04:16:04 +0200

    appel api en cours

--- src/Compoments/Home.jsx
+++ src/Compoments/Home.jsx
@@ -1,11 +1,29 @@
+import axios from 'axios'
 import React from 'react'
 
 export default function Home() {
 
-const getWeather = () =>  {
+  const APIKEY = process.env.REACT_APP_API_KEY
 
-}
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
 
+      //console.log(position)
+
+      let long = position.coords.longitude;
+      let lat = position.coords.latitude
+
+      const getWeather = (long, lat) => {
+        axios.get (`https://api.openweathermap.org/data/3.0/onecall?${lat}&${long}.04&exclude=minutely,alerts&appid=${APIKEY}`)
+        .then ((response) => {
+          const data = response
+          
+        })
+      }
+    })
+  } else {
+    alert('Vous devez accepter la géolocalisation pour utiliser l\'app!')
+  }
   return (
     <div className='home'>
       <h1 className='city'>
